415c0f52dea162c7fe07ca37a3b1f3a0ad1fe78f
Benjamin-Re <renker.benjamin@gmail.com>
2022-04-01 19:21:38 +0200

    Add exercise 5 and start with 6

--- algorithms3.js
+++ algorithms3.js
@@ -77,9 +77,8 @@ function rainbowLog() {
 /*Exercise 4*/
 // Ask the user for a DNI
 
-function DniValidator() {
-    let dni = prompt("DNI please");
-
+function dniValidator(dni) {
+    
     const validDni = [];
 
     // Validate the DNI and inform the user of the validation 
@@ -99,4 +98,84 @@ function DniValidator() {
     }
 }
 
- 
+/* Exercise 4 extra - random DNI */
+// Create random valid DNI
+function dniCreator() {
+  let dni = "";
+  for (let i = 0; i < 8; i++) {
+    dni += randomGenerator(0, 9);
+  }
+  // Add the apropriate character to the end
+  let rest = Number(dni.substring(0, 8)) % 23;
+  const restTable = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "H", "L", "C", "K", "E"];
+  dni += restTable[rest];
+  console.log(dni);
+  // Check if our random dni is really valid
+  dniValidator(dni);
+}
+
+/*Exercis 5 -create matriculas(license plate?)*/
+// A license plate consists of 4 numbers and 3 letters
+// No vocals or Q allowed
+
+function generateMatriculas(quantity) {
+  for(let j=0; j<quantity; j++){
+    let matricula = "";
+    let chars = "";
+    let nums = "";
+    const validChars = ["B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    for (let i = 0; i < 4; i++) {
+      chars += validChars[randomGenerator(0, 21)];
+    }
+    for (let i = 0; i < 3; i++) {
+      nums += randomGenerator(0, 9);
+    }
+    // Add both strings chars and nums
+    chars += nums;
+  
+    // Mix together the random legal chars and nums
+    for (let i = 0; i < 7; i++) {
+      matricula += chars[randomGenerator(0, 6)];
+    }
+    console.log(matricula);
+  }
+}
+
+/*Exercise 6 - dates*/
+
+function printDate() {
+
+// create 20 dates of various themes (programming, work out, haircut, ...)
+// Save the dates in an array
+  let dateArray = ["Time for your workout!",
+    "Job interview, good luck!",
+    "Programming bootcamp",
+    "Get down to the barber for a haircut",
+    "Time for a break",
+    "Shopping for groceries",
+    "Pick up your daughter from Kindergarden",
+    "Another workout go go go",
+    "Step-mothers birthday, dont forget present",
+    "Technician coming over to fix Wifi",
+    "Get a booster shot at Centro de Salud",
+    "Get a nap zzzzzzzz",
+    "Clean up",
+    "Wipe the floor",
+    "Do laundry, again...",
+    "Car is dirty",
+    "Car is broken",
+    "Go to the doctor for a blood analysis",
+    "Take a PCR test",
+    "Bring kiddo to pediatrician"];
+
+  // Print a random date to the console
+  console.log(dateArray[randomGenerator(0, 19)]);
+}
+
+// Print a date every 10 seconds
+// Using setInterval to call the function in an interval of 10 seconds
+let myIntervall = setInterval(printDate, 10000);
+
+// Stop printing dates after 2 minutes using setTimeout to clear the Interval
+// after 2 minutes
+// setTimeout(()=>{clearInterval(myInterval)}, 120000);
