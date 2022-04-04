092ee8513422706c84b1ea7ae39b1dea86d7af70
Benjamin-Re <renker.benjamin@gmail.com>
2022-04-04 19:13:04 +0200

    i dont know what git wants now

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-let headersAndParagraphs = document.querySelectorAll("h1,p");
-console.log(headersAndParagraphs[2].innerText);
+// let headersAndParagraphs = document.querySelectorAll("h1,p");
+
+
+// let nav = document.querySelector("nav");
+
+
+// console.log(headersAndParagraphs[0].firstChild);
+// console.log(headersAndParagraphs[0].firstChild.nodeValue);
+// console.log(nav.nodeName);
+
+let box1 = document.querySelector("section").firstElementChild;
+let box2 = box1.nextElementSibling;
+let box3 = box2.nextElementSibling;
+
+box1.classList.add("disappear");
+box1.classList.remove("dark");
